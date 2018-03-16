12fa15109c23f6861a32d0a34092c0036f0a38a6
Igor Fritzsch <igor.fritzsch@gmail.com>
2018-03-16 22:44:12 +0100

    Alert when threshold has been reached

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 var Fritz = require('fritzapi').Fritz;
-
 var fritz = new Fritz(process.env.USERNAME, process.env.PASSWORD);
 
-// utility function to sequentialize promises
+var devices = {};
+var alert = process.env.ALERT && JSON.parse(process.env.ALERT.toLowerCase()) || false;
+var interval = process.env.INTERVAL || 10000;
+var threshold = process.env.THRESHOLD || 15000;
+
+/* Utility function to sequentialize promises */
 function sequence(promises) {
 	var result = Promise.resolve();
 	promises.forEach(function(promise,i) {
@@ -14,41 +18,35 @@ function sequence(promises) {
 	return result;
 }
 
-function switches() {
-	return fritz.getSwitchList().then(function(switches) {
-		console.log("Switches: " + switches + "\n");
-
-		return sequence(switches.map(function(sw) {
-			return function() {
-				return sequence([
-					function() {
-						return fritz.getSwitchName(sw).then(function(name) {
-							console.log("[" + sw + "] " + name);
-						});
-					},
-					function() {
-						return fritz.getSwitchPresence(sw).then(function(presence) {
-							console.log("[" + sw + "] presence: " + presence);
-						});
-					},
-					function() {
-						return fritz.getSwitchState(sw).then(function(state) {
-							console.log("[" + sw + "] state: " + state);
-						});
-					},
-					function() {
-						return fritz.getTemperature(sw).then(function(temp) {
-							temp = isNaN(temp) ? '-' : temp + "°C";
-							console.log("[" + sw + "] temp: " + temp + "\n");
-						});
-					}
-				]);
-			};
-		}));
-	});
+/* Alert when threshold has been reached. */
+if (alert) {
+	setInterval(function () {
+		return fritz.getSwitchList().then(function(switches) {
+			return sequence(switches.map(function(sw) {
+				devices[sw] = devices[sw] || {};
+				return 	function() {
+					fritz.getSwitchPresence(sw).then(function(presence) {
+						if (presence) {
+							fritz.getSwitchPower(sw).then(function(pow) {
+								pow = isNaN(pow) ? '-' : pow + "W";
+								if (!devices[sw]['alert'] && pow >= threshold) {
+									fritz.getSwitchName(sw).then(function(name) {
+										devices[sw]['alert'] = true;
+										console.log("[" + Date.now() + " | " + sw + "] " + name + "\'s power is above threshold: " + pow);
+									});
+								}
+								else if (pow < threshold) {
+									devices[sw]['alert'] = false;
+									console.log("["  + Date.now() + " | " + sw + "] power is: " + pow);
+								}
+							});
+						}
+					});
+				}
+			}));
+		});
+	}, interval);
 }
-
-switches();
 
 /* GET devices. */
 router.get('/devices', function(req, res, next) {
