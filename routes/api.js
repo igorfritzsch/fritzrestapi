d1a8f5d802f705547410244db8d405bdc8fb4bee
Igor Fritzsch <igor.fritzsch@gmail.com>
2018-03-19 12:00:52 +0100

    Schedule alerts

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,23 @@
 var express = require('express');
 var request = require('request');
+var schedule = require('node-schedule');
+
 var router = express.Router();
 
 var Fritz = require('fritzapi').Fritz;
 var fritz = new Fritz(process.env.USERNAME, process.env.PASSWORD);
 
 var devices = {};
+var timer = null;
 var alert = process.env.ALERT && JSON.parse(process.env.ALERT.toLowerCase()) || false;
 var interval = process.env.INTERVAL || 10000;
 var threshold = process.env.THRESHOLD || 15000;
 var iftttKey = process.env.IFTTT_KEY;
 var iftttEventOn = process.env.IFTTT_EVENT_ON;
 var iftttEventOff = process.env.IFTTT_EVENT_OFF;
+
+var startTime = process.env.STARTTIME && process.env.STARTTIME.split(':').map(function(time){return parseInt(time)}) || [18,0];
+var endTime = process.env.STARTTIME && process.env.ENDTIME.split(':').map(function(time){return parseInt(time)}) || [6,0];
 
 /* Utility function to sequentialize promises */
 function sequence(promises) {
@@ -41,36 +47,58 @@ function sendNotification(params) {
 
 /* Alert when threshold has been reached. */
 if (alert) {
-	setInterval(function () {
-		return fritz.getSwitchList().then(function(switches) {
-			return sequence(switches.map(function(sw) {
-				devices[sw] = devices[sw] || {};
-				return 	function() {
-					fritz.getSwitchPresence(sw).then(function(presence) {
-						if (presence) {
-							fritz.getSwitchPower(sw).then(function(pow) {
-								pow = isNaN(pow) ? '-' : pow + "W";
-								if (!devices[sw]['alert'] && pow >= threshold) {
-									fritz.getSwitchName(sw).then(function(name) {
-										devices[sw]['alert'] = true;
-										console.log("[" + Date.now() + " | " + sw + "] " + name + "\'s power is above threshold: " + pow);
-										if (iftttEventOn)
-											sendNotification({event: iftttEventOn, values: {value1: name, value2: pow}});
-									});
-								}
-								else if (devices[sw]['alert'] && pow < threshold) {
-									devices[sw]['alert'] = false;
-									console.log("["  + Date.now() + " | " + sw + "] power is: " + pow);
-									if (iftttEventOff)
-										sendNotification({event: iftttEventOff, values: {value1: name, value2: pow}});
-								}
-							});
-						}
-					});
-				}
-			}));
-		});
-	}, interval);
+
+	/* Start alert */
+	var startInterval = function () {
+		timer = setInterval(function () {
+			return fritz.getSwitchList().then(function(switches) {
+				return sequence(switches.map(function(sw) {
+					devices[sw] = devices[sw] || {};
+					return 	function() {
+						fritz.getSwitchPresence(sw).then(function(presence) {
+							if (presence) {
+								fritz.getSwitchPower(sw).then(function(pow) {
+									pow = isNaN(pow) ? '-' : pow + "W";
+									if (!devices[sw]['alert'] && pow >= threshold) {
+										fritz.getSwitchName(sw).then(function(name) {
+											devices[sw]['alert'] = true;
+											console.log("[" + Date.now() + " | " + sw + "] " + name + "\'s power is above threshold: " + pow);
+											if (iftttEventOn)
+												sendNotification({event: iftttEventOn, values: {value1: name, value2: pow}});
+										});
+									}
+									else if (devices[sw]['alert'] && pow < threshold) {
+										devices[sw]['alert'] = false;
+										console.log("["  + Date.now() + " | " + sw + "] power is: " + pow);
+										if (iftttEventOff)
+											sendNotification({event: iftttEventOff, values: {value1: name, value2: pow}});
+									}
+								});
+							}
+						});
+					}
+				}));
+			});
+		}, interval);
+	};
+
+	/* Stop alert */
+	var stopInterval = function () {
+		clearInterval(timer);
+	};
+
+	/* Start alert at default: 6:00pm */
+	var alertStart = schedule.scheduleJob({hour: startTime[0], minute: startTime[1]}, function(){
+		console.log('Start Alert!');
+		startInterval();
+	});
+
+	/* Stop alert at default: 6:00am */
+	var alertEnd = schedule.scheduleJob({hour: endTime[0], minute: endTime[1]}, function(){
+		console.log('Stop Alert!');
+		stopInterval();
+	});
+
 }
 
 /* GET devices. */
